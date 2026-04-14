06b6a0660cef6d66ce52f6f6293b907610296f4c
root <root@LAPTOP-4VJGB7NM.localdomain>
2026-04-14 19:32:42 +0800

    0414_some emo

---
+++ push.sh
@@ -0,0 +1,11 @@
+# A
+
+git push -u origin master
+
+#B
+
+# 1. 新建并切换到新分支（把 dev 改成你想要的名字，比如 feature/login） ^-^    Create and switch to a new branch (change dev to the name you want, such as feature/login)
+git checkout -b xsm_666
+
+# 2. 推送到远程 GitHub，并建立关联
+git push -u origin xsm_666
